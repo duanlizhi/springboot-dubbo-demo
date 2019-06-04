55f77531202c4914ecc4bfdca0fb76a8be551ae7
段lizhi <lizhi_duan@126.com>
2019-06-04 23:41:34 +0800

    firstcommit

---
+++ springboot-opay-provider/src/main/java/com/opay/SpringBootOpayProviderApplication.java
@@ -0,0 +1,13 @@
+package com.opay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpayProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpayProviderApplication.class, args);
+    }
+
+}
